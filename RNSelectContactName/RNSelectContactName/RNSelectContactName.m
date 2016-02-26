13dd4bf059944c167006c7934e26fae30f6d7f9f
Ross Haker <you@example.com>
2016-02-26 09:58:59 -0800

    updated reject function for RN > 19

--- RNSelectContactName/RNSelectContactName/RNSelectContactName.m
+++ RNSelectContactName/RNSelectContactName/RNSelectContactName.m
@@ -18,10 +18,10 @@ RCT_EXPORT_METHOD(selectName:(BOOL *)boolType
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    
+
     // save the resolve promise
     self.resolve = resolve;
-    
+
     // set up an error message
     NSError *error = [
                       NSError errorWithDomain:@"some_domain"
@@ -29,23 +29,23 @@ RCT_EXPORT_METHOD(selectName:(BOOL *)boolType
                       userInfo:@{
                                  NSLocalizedDescriptionKey:@"ios8 or higher required"
                                  }];
-    
-    
+
+
     // detect the ios version
     NSString *ver = [[UIDevice currentDevice] systemVersion];
     float ver_float = [ver floatValue];
-    
+
     // check that ios is version 8.0 or higher
     if (ver_float < 8.0) {
-        
-        reject(error);
-        
+
+        reject(@"200", @"ios8 or higher required", error);
+
     } else {
-        
+
         // check permissions
         if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusDenied ||
             ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusRestricted){
-            
+
             // permission denied
             error = [
                      NSError errorWithDomain:@"some_domain"
@@ -53,26 +53,26 @@ RCT_EXPORT_METHOD(selectName:(BOOL *)boolType
                      userInfo:@{
                                 NSLocalizedDescriptionKey:@"Permissions denied by user."
                                 }];
-            
-            reject(error);
-            
+
+            reject(@"300", @"Permissions denied by user.", error);
+
         } else if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusAuthorized){
-            
+
             // permission authorized
             ABPeoplePickerNavigationController *picker;
             picker = [[ABPeoplePickerNavigationController alloc] init];
             picker.peoplePickerDelegate = self;
-            
+
             UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
             [vc presentViewController:picker animated:YES completion:nil];
-            
+
         } else {
-            
+
             // not determined - request permissions
             ABAddressBookRequestAccessWithCompletion(ABAddressBookCreateWithOptions(NULL, nil), ^(bool granted, CFErrorRef error) {
-                
+
                 if (!granted){
-                    
+
                     // user denied access
                     NSError *errorDenied = [
                                             NSError errorWithDomain:@"some_domain"
@@ -80,77 +80,77 @@ RCT_EXPORT_METHOD(selectName:(BOOL *)boolType
                                             userInfo:@{
                                                        NSLocalizedDescriptionKey:@"Permissions denied by user."
                                                        }];
-                    
-                    reject(errorDenied);
+
+                    reject(@"300", @"Permissions denied by user.", errorDenied);
                     return;
                 }
-                
+
                 // user authorized access
                 ABPeoplePickerNavigationController *picker;
                 picker = [[ABPeoplePickerNavigationController alloc] init];
                 picker.peoplePickerDelegate = self;
-                
+
                 UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
                 [vc presentViewController:picker animated:YES completion:nil];
-                
+
             });
-            
+
         }
-        
+
     }
-    
-    
+
+
 }
 
 - (void)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker didSelectPerson:(ABRecordRef)person
 {
-    
+
     // get the first name
     NSString *firstName = (__bridge_transfer NSString *)ABRecordCopyValue(person, kABPersonFirstNameProperty);
-    
+
     //get the middle name
     NSString *middleName = (__bridge_transfer NSString*)ABRecordCopyValue(person, kABPersonMiddleNameProperty);
-    
+
     // get the last name
     NSString *lastName = (__bridge_transfer NSString *)ABRecordCopyValue(person, kABPersonLastNameProperty);
-    
+
     // set the return strings
     NSString* returnFirstName;
     NSString* returnLastName;
     NSString* returnMiddleName;
-    
+
     // check for null values
     if (firstName) {
         returnFirstName = firstName;
     } else {
         returnFirstName = @"";
     }
-    
+
     // check for null values
     if (lastName) {
         returnLastName = lastName;
     } else {
         returnLastName = @"";
     }
-   
+
     // check for null values
     if (middleName) {
         returnMiddleName = middleName;
     } else {
         returnMiddleName = @"";
     }
-    
+
     // Set the return dictionary
     NSDictionary *resultsDict = @{
                                   @"firstName" : returnFirstName,
                                   @"middleName" : returnMiddleName,
                                   @"lastName" : returnLastName,
                                   };
-    
-    
+
+
     UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
     [vc dismissViewControllerAnimated:YES completion:nil];
-    
+
     // resolve the name
     self.resolve(resultsDict);
 }
@@ -160,7 +160,7 @@ RCT_EXPORT_METHOD(selectName:(BOOL *)boolType
 }
 
 -(void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker{
-    
+
     UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
     [vc dismissViewControllerAnimated:YES completion:nil];
 }
